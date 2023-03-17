e12c1b98c3528b496d80a7206244fdbf491461b6
Abdul Rahim Kattirithodi <abdulrahim.uj@gmail.com>
2023-03-18 02:15:36 +0530

    fix user dashboard issues

--- base/views.py
+++ base/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from submissions.models import Department
 
 
 def home(request):
+    departments = Department.objects.filter(is_deleted=False, is_visible=True).order_by('department_name')
     context = {
         'title': "Home",
         'is_bootstrap': True,
+        'departments': departments,
     }
     return render(request, 'home/home.html', context)
